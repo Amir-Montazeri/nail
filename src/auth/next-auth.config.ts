d6da7dff1c75b8c9c809d37132773aa9ee4f465b
Amir Montazeri <m3amir88@yahoo.com>
2024-06-15 22:44:31 +0330

    google auth configured!

--- src/auth/next-auth.config.ts
+++ src/auth/next-auth.config.ts
@@ -1,11 +1,16 @@
 import type { NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import Google from 'next-auth/providers/google';
 import bcrypt from 'bcryptjs';
 import { LoginSchema } from '@/schemas';
 import { getUserByEmail } from '@db-modules/user';
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validateedFields = LoginSchema.safeParse(credentials);
